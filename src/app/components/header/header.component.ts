ef95c9ffe73961f655479ee8858c2a5d2edd686b
kevinsmasters <gmail@kevinmasters.com>
2023-04-15 15:02:41 -0400

    implement cart functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  private _cart: Cart = { items: [] }
 
-  ngOnInit(): void {
+  itemsQuantity = 0;
+
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
+  set cart(cart: Cart) {
+    this._cart = cart;
+    this.itemsQuantity = cart.items
+      .map((item) => item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+  constructor(private cartService: CartService) { }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items)
   }
 
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
 }
